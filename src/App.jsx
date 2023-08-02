15c6875f53e1a28bdb4ec3b5a8128f3d0fb82ce8
PasinPeat <pasin.lawn1995@gmail.com>
2023-08-02 22:21:31 +0700

    Feat:add topInput Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LeftSidebar from "./component/leftSidebar";
 import Introduce from "./component/Introduce";
 import OtherProject from "./component/OtherProject";
-import Skill from "./component/Skill";
+import Post from "./component/Post";
+import TopInput from "./component/TopInput";
 function App() {
   return (
     <div className="LandingPage">
       <LeftSidebar />
-      
+
       <div className="CenterContent">
+        <TopInput/>
         <Introduce />
         <OtherProject />
       </div>
-      <Skill/>
+      <Post />
     </div>
   );
 }
--- src/component/TopInput.jsx
+++ src/component/TopInput.jsx
@@ -1,14 +1,47 @@
 import { useState } from "react";
 import React from "react";
 
+function TopInput() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
 
-function TopInput(){
+  function handleAddPost(e) {
+    e.preventDefault();
+    const newPosts = [...posts];
+    newPosts.push(input);
+    setPosts(newPosts);
+    setInput("");
+  }
 
-    const[input,setInput] = useState("")
-
-    return(
-        <div className="Input">
-        <div className="Input_header">Create Post</div>
-        </div>
-    )
+  return (
+    <div className="Input">
+      <div className="Input_header">Say something to me</div>
+      <input
+        className="Input_field"
+        type="text"
+        placeholder="What's on your mind ?"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        value={input}
+        onClick={handleAddPost}
+        type="submit"
+        className="button_post"
+      >
+        Click
+      </button>
+      <div>
+        {posts.map((item, index) => {
+          return (
+            <div key={index} className="message">
+              <h1>{item}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+export default TopInput;
