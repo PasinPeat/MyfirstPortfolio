3523f6e95ca78790c02c4e53ee2bf943bfaa9400
PasinPeat <pasin.lawn1995@gmail.com>
2023-08-04 06:36:24 +0700

    Resolve: deploy conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Introduce from "./pages/Introduce";
-import OtherProject from "./pages/OtherProject";
-import Passion from "./pages/Passion";
+
 import Post from "./component/Post";
-import TopInput from "./component/TopInput";
-import LeftSidebar from "./component/leftSidebar";
+
+// import LeftSidebar from "./component/leftSidebar";
 import CenterContent from "./component/CenterContent";
 import { MessageContext } from "./context/MessageContext";
 
@@ -22,10 +19,8 @@ function App() {
           { posts, setPosts },
         ]}
       >
-        <LeftSidebar />
-        <CenterContent/>
-        
-
+        {/* <LeftSidebar /> */}
+        <CenterContent />
         <Post />
       </MessageContext.Provider>
     </div>
